24bcefcf6652637b1d08d53da6486df4b2e21455
LimJiNa <jn4624@gmail.com>
2023-09-04 23:05:27 +0900

    initial commit

---
+++ src/main/java/com/app/feign/FeignProjApplication.java
@@ -0,0 +1,13 @@
+package com.app.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignProjApplication.class, args);
+    }
+
+}
